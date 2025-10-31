4ebbec0102a4d8d109a31a26491e62115a4bf40b
Sam Vishnevskiy <sam.vishnevskiy@gmail.com>
2025-10-31 13:22:31 -0500

    Added engineered features as new model parameters

--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import glob
 import os
 import torch
@@ -19,21 +20,22 @@ csv_files = glob.glob(os.path.join(data_folder, "*.csv"))
 
 df_list = [pd.read_csv(file) for file in csv_files]
 df = pd.concat(df_list, ignore_index=True)
-
-#There were some errors with the original code leading to nans, so this removes the nans
-df = df.replace([float('inf'), -float('inf')], 0)  
-df = df.dropna()                                  
-
+                               
 print(f"Loaded {len(csv_files)} files with {len(df)} rows total")
+df['C_IV_PREV'] = df['C_IV'].shift(1)
+df['C_IV_ROLL_MEAN_5'] = df['C_IV'].rolling(window=5).mean()
+df = df.replace([float('inf'), -float('inf')], 0)  
+df = df.dropna()   
 print(df.head())
 print(df.shape)
 
 inputs = [
-    'UNDERLYING_LAST', 'STRIKE', 'DTE',
-    'C_DELTA','C_GAMMA','C_VEGA','C_THETA','C_RHO',
-    'P_DELTA','P_GAMMA','P_VEGA','P_THETA','P_RHO',
-    'C_VOLUME','P_VOLUME'
-]
+    'UNDERLYING_LAST', 'DTE', 'C_DELTA', 'C_GAMMA', 'C_VEGA', 'C_THETA', 
+    'C_RHO', 'C_VOLUME', 'C_LAST', 'C_BID', 'C_ASK', 'STRIKE',
+    'P_BID', 'P_ASK', 'P_LAST', 'P_DELTA', 'P_GAMMA', 'P_VEGA', 'P_THETA', 
+    'P_RHO', 'P_IV', 'P_VOLUME', 'STRIKE_DISTANCE', 'STRIKE_DISTANCE_PCT', 'C_IV_PREV',
+    'C_IV_ROLL_MEAN_5',
+    ]
 
 target = 'C_IV'
 scaler_x = StandardScaler()
@@ -45,26 +47,28 @@ X = scaler_x.fit_transform(X)
 xtensor = torch.tensor(X, dtype = torch.float32).to(device)
 ytensor = torch.tensor(Y, dtype = torch.float32).to(device)
 
-#this model is the function that computes the implied volatility from the 15 inputs
+#this model is the function that computes the implied volatility from the inputs
 #There are 4 layers to the model
 #inbetween the linear layers, a rectified linear unit is applied to the previous output, which converts negative values
 # in the matricies to 0, introducing non-linearity which allows for the model to represent more complex functions
-model = nn.Sequential(nn.Linear(15, 256), nn.ReLU(), nn.Linear(256, 128), nn.ReLU(), nn.Linear(128, 32), nn.ReLU(), nn.Linear(32,1)).to(device)
+model = nn.Sequential(nn.Linear(len(inputs), 256), nn.ReLU(), nn.Linear(256, 128), nn.ReLU(), nn.Linear(128, 32), nn.ReLU(), nn.Linear(32,1)).to(device)
 
 loss_function = nn.MSELoss()                           #mean-squared error
 optimizer = optim.SGD(model.parameters(), lr = 0.05)       #optimization mechanism, stochastic gradient descent, can experiment with learning rate later
 
 dataset = TensorDataset(xtensor, ytensor)
-#loader = DataLoader(dataset, batch_size = 1000, shuffle=True)  this is from when I was using batching
-num_epochs = 200      #number of learning cycles, can vary this in the future
+#loader = DataLoader(dataset, batch_size = 1000, shuffle=True)    # this is from when I was using batching
+num_epochs = 1000     #number of learning cycles, can vary this in the future
 
 for epoch in range(num_epochs):
+    
     optimizer.zero_grad()                                 #clear old gradients
     loss = loss_function(model(xtensor), ytensor)       #calculate the loss at this step
     loss.backward()                                      #use back-propogation to calculate gradients for all model weights
     optimizer.step()                                    #adjusts the weights using these gradients
-    if(epoch % 10 == 0):
+    if(epoch % 50 == 0):
         print(f"Epoch {epoch}/{num_epochs} Loss:{loss.item()}")
+    
 
     """
     #I decided to experiment with batching to make the model faster, 
@@ -78,6 +82,6 @@ for epoch in range(num_epochs):
         loss.backward()                             
         optimizer.step()                    
         total_loss += loss.item()
-        total_loss/=len(loader)
+    total_loss/=len(loader)
     print(f"Epoch {epoch}/{num_epochs} Loss:{total_loss}")
     """
